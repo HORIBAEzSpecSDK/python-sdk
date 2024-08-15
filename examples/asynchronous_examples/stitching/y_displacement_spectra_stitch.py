518009084a344b97f1543c0d722a77d33efa2009
Samuel Gauthier <work.samuel.gauthier@gmail.com>
2024-08-15 14:36:48 +0200

    add forgotten y displacement stitching

--- examples/asynchronous_examples/stitching/y_displacement_spectra_stitch.py
+++ examples/asynchronous_examples/stitching/y_displacement_spectra_stitch.py
@@ -1,16 +1,16 @@
 from typing import Any, List
 
 from loguru import logger
-from numpy import argsort, array, concatenate, dtype, interp, ndarray
+from numpy import array, concatenate, dtype, ndarray
 from overrides import override
 
 from examples.asynchronous_examples.stitching.spectra_stitch import SpectraStitch
 
 
-class LinearSpectraStitch(SpectraStitch):
+class YDisplacementSpectraStitch(SpectraStitch):
     """Stiches a list of spectra using a linear model"""
 
-    def __init__(self, spectra_list: List[List[List[float]]]) -> None:
+    def __init__(self, spectrum1: List[List[float]], spectrum2: List[List[float]], y_displacement_count: int) -> None:
         """Constructs a linear stitch of spectra.
 
         .. warning:: The spectra in the list must overlap
@@ -18,11 +18,10 @@ class LinearSpectraStitch(SpectraStitch):
         Parameters
             spectra_list : List[List[List[float]]] List of spectra to stitch in the form [[x1_values, y1_values],
             [x2_values, y2_values], etc].
+            y_displacement_count : int The amount of displacement in the y direction for the second spectrum
         """
-        stitched_spectrum = spectra_list[0]
-
-        for i in range(1, len(spectra_list)):
-            stitched_spectrum = self._stitch_spectra(stitched_spectrum, spectra_list[i])
+        self._y_displacement_count = y_displacement_count
+        stitched_spectrum = self._stitch_spectra(spectrum1, spectrum2)
 
         self._stitched_spectrum: List[List[float]] = stitched_spectrum
 
@@ -36,7 +35,7 @@ class LinearSpectraStitch(SpectraStitch):
         Returns:
             SpectraStitch: The stitched spectra.
         """
-        new_stitch = LinearSpectraStitch([self.stitched_spectra(), other_stitch.stitched_spectra()])
+        new_stitch = YDisplacementSpectraStitch([self.stitched_spectra(), other_stitch.stitched_spectra()])
         return new_stitch
 
     @override
@@ -64,18 +63,25 @@ class LinearSpectraStitch(SpectraStitch):
             logger.error(f'No overlap between two spectra: {spectrum1}, {spectrum2}')
             raise Exception('No overlapping region between spectra')
 
+        # Masks for overlapping region
         mask1 = (x1 >= overlap_start) & (x1 <= overlap_end)
         mask2 = (x2 >= overlap_start) & (x2 <= overlap_end)
 
-        y2_interp = interp(x1[mask1], x2[mask2], y2[mask2])
+        x1_overlap = x1[mask1]
+        y1_overlap = y1[mask1]
 
-        y_combined_overlap = (y1[mask1] + y2_interp) / 2
+        y2_displaced = y2 + self._y_displacement_count
+        y2_overlap = y2_displaced[mask2]
 
-        x_combined = concatenate((x1[~mask1], x1[mask1], x2[~mask2]))
-        y_combined = concatenate((y1[~mask1], y_combined_overlap, y2[~mask2]))
+        y_stitched_overlap = (y1_overlap + y2_overlap) / 2
 
-        sort_indices = argsort(x_combined)
-        x_combined = x_combined[sort_indices]
-        y_combined = y_combined[sort_indices]
+        x1_before_overlap = x1[x1 < overlap_start]
+        y1_before_overlap = y1[x1 < overlap_start]
 
-        return [x_combined.tolist(), y_combined.tolist()]
+        x2_after_overlap = x2[x2 > overlap_end]
+        y2_after_overlap = y2_displaced[x2 > overlap_end]
+
+        x_stitched = concatenate([x1_before_overlap, x1_overlap, x2_after_overlap])
+        y_stitched = concatenate([y1_before_overlap, y_stitched_overlap, y2_after_overlap])
+
+        return [x_stitched.tolist(), y_stitched.tolist()]
