c0ef6f03ebc9bfb03cf36419b588e31177ed5951
ads\arbh <bharath.arun@zuehlke.com>
2024-11-18 12:47:35 +0100

    added file for dark count subtraction example

---
+++ examples/asynchronous_examples/ccd_dark_count_subtraction_example.py
@@ -0,0 +1,69 @@
+import asyncio
+import random
+
+from loguru import logger
+
+from horiba_sdk.core.x_axis_conversion_type import XAxisConversionType
+from horiba_sdk.devices.device_manager import DeviceManager
+
+
+async def main():
+    device_manager = DeviceManager(start_icl=True)
+    await device_manager.start()
+
+    if not device_manager.charge_coupled_devices:
+        logger.error('No CCDs found, exiting...')
+        await device_manager.stop()
+        return
+
+    ccd = device_manager.charge_coupled_devices[0]
+    await ccd.open()
+
+    try:
+        await ccd.set_acquisition_count(1)
+        await ccd.set_x_axis_conversion_type(XAxisConversionType.NONE)
+        await ccd.set_acquisition_abort()
+        logger.info(await ccd.get_acquisition_count())
+        logger.info(await ccd.get_clean_count())
+        logger.info(await ccd.get_timer_resolution())
+        logger.info(await ccd.get_gain())
+        logger.info(await ccd.get_chip_size())
+        logger.info(await ccd.get_exposure_time())
+        await ccd.set_exposure_time(random.randint(1, 5))
+        logger.info(await ccd.get_exposure_time())
+        logger.info(await ccd.get_temperature())
+        await ccd.set_region_of_interest()  # Set default ROI, if you want a custom ROI, pass the parameters
+        logger.info(await ccd.get_speed())
+        if await ccd.get_acquisition_ready():
+            await ccd.set_acquisition_start(open_shutter=False)
+            await asyncio.sleep(1)  # Wait a short period for the acquisition to start
+            # Poll for acquisition status
+            acquisition_busy = True
+            while acquisition_busy:
+                acquisition_busy = await ccd.get_acquisition_busy()
+                await asyncio.sleep(0.3)
+                logger.info('Acquisition busy')
+
+            data_shutter_closed = await ccd.get_acquisition_data()
+            logger.info(f"Data with closed shutter: {data_shutter_closed}")
+
+        if await ccd.get_acquisition_ready():
+            await ccd.set_acquisition_start(open_shutter=True)
+            await asyncio.sleep(1)  # Wait a short period for the acquisition to start
+            # Poll for acquisition status
+            acquisition_busy = True
+            while acquisition_busy:
+                acquisition_busy = await ccd.get_acquisition_busy()
+                await asyncio.sleep(0.3)
+                logger.info('Acquisition busy')
+
+            data_shutter_open = await ccd.get_acquisition_data()
+            logger.info(f"Data with open shutter: {data_shutter_open}")
+    finally:
+        await ccd.close()
+
+    await device_manager.stop()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
