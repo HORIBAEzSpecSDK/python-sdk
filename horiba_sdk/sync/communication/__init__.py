39ed1df21c32235925a772fc46f594d169244742
ads\arbh <bharath.arun@zuehlke.com>
2025-03-13 11:55:01 +0100

    removed sync related code, examples and tests

--- horiba_sdk/sync/communication/__init__.py
+++
@@ -1,7 +0,0 @@
-from .abstract_communicator import AbstractCommunicator
-from .websocket_communicator import WebsocketCommunicator
-
-__all__ = [
-    'AbstractCommunicator',
-    'WebsocketCommunicator',
-]
