29d163d89a24229cd4dcf8322ea73f948eddd4f8
bharathZE <109354167+bharathZE@users.noreply.github.com>
2025-03-24 12:03:43 +0100

    Removal of the sync supporting code (#13)
    * removed sync related code, examples and tests
    
    * corrected typo
    
    * removed unit tests related to sync feature in conftest.py

--- horiba_sdk/sync/devices/single_devices/__init__.py
+++
@@ -1,5 +0,0 @@
-from .abstract_device import AbstractDevice
-from .ccd import ChargeCoupledDevice
-from .monochromator import Monochromator
-
-__all__ = ['AbstractDevice', 'Monochromator', 'ChargeCoupledDevice']
