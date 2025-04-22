787fd25f23694755e011e593ba4e1fa6b1ab4059
DESKTOP-R7JVAOO\Dell_Z84534 <nikolaus.naredi-rainer@zuehlke.com>
2025-04-22 09:42:22 +0200

    fix: enable waiting for saq3 acquisition before data collection

--- examples/spectracq3/spectraq3_range_scan.py
+++ examples/spectracq3/spectraq3_range_scan.py
@@ -21,7 +21,7 @@ async def main():
     await wait_for_mono(mono)
     spectracq3 = device_manager.spectracq3_devices[0]
     await spectracq3.open()
-    # await wait_for_saq3(spectracq3)
+    await wait_for_saq3(spectracq3)
 
     start_wavelength = 490
     end_wavelength = 520
@@ -41,24 +41,22 @@ async def main():
                 await asyncio.sleep(0.1)
             logger.info(f'Monochromator set to {wavelength}nm')
 
-            # if not await spectracq3.is_busy():
+            if not await spectracq3.is_busy():
 
-            await spectracq3.acq_start(1)
-            await asyncio.sleep(3)
-            data = await spectracq3.get_available_data()
-            logger.info(f'Acquired data at {wavelength}nm: {data}')
-            x_data.append(wavelength)
-            y_data_current.append(data[0]['currentSignal']['value'])
-            y_data_voltage.append(data[0]['voltageSignal']['value'])
-            y_data_counts.append(data[0]['ppdSignal']['value'])
+                await spectracq3.acq_start(1)
+                await asyncio.sleep(3)
+                data = await spectracq3.get_available_data()
+                logger.info(f'Acquired data at {wavelength}nm: {data}')
+                x_data.append(wavelength)
+                y_data_current.append(data[0]['currentSignal']['value'])
+                y_data_voltage.append(data[0]['voltageSignal']['value'])
+                y_data_counts.append(data[0]['ppdSignal']['value'])
 
     finally:
         await mono.close()
         await spectracq3.close()
         await device_manager.stop()
 
-    # await plot_values(start_wavelength, end_wavelength, spectrum)
-    # if you need to plot voltage or counts, change the y_data_current to y_data_voltage or y_data_counts
     await plot_values(start_wavelength, end_wavelength, x_data, y_data_current)
 
 
@@ -72,12 +70,12 @@ async def plot_values(start_wavelength, end_wavelength, x_data, y_data):
     plt.show()
 
 
-# async def wait_for_saq3(saq3):
-#     acquisition_busy = True
-#     while acquisition_busy:
-#         acquisition_busy = await saq3.is_busy()
-#         await asyncio.sleep(1)
-#         logger.info('Acquisition busy')
+async def wait_for_saq3(saq3):
+    acquisition_busy = True
+    while acquisition_busy:
+        acquisition_busy = await saq3.is_busy()
+        await asyncio.sleep(1)
+        logger.info('Acquisition busy')
 
 
 async def wait_for_mono(mono):
