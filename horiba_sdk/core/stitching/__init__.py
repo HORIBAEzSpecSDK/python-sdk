cd94318d04e397498b192a57d1174e44a9b27f08
peter-bartalis <peter.bartalis@horiba.com>
2025-10-09 11:25:38 -0400

    Update __init__.py
    style fixes

--- horiba_sdk/core/stitching/__init__.py
+++ horiba_sdk/core/stitching/__init__.py
@@ -4,4 +4,5 @@ from .simple_cut_spectra_stitch import SimpleCutSpectraStitch
 from .spectra_stitch import SpectraStitch
 from .y_displacement_spectra_stitch import YDisplacementSpectraStitch
 
-__all__ = ['LabSpec6SpectraStitch', 'LinearSpectraStitch', 'SimpleCutSpectraStitch', 'YDisplacementSpectraStitch', 'SpectraStitch']
+__all__ = ['LabSpec6SpectraStitch', 'LinearSpectraStitch', 'SimpleCutSpectraStitch', 
+           'YDisplacementSpectraStitch', 'SpectraStitch']
